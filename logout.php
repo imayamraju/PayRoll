014661e397d4f6bb4858dfc8a709d90c3ed4378e
Mahinth Christensen <mahinthjoe@gmail.com>
2011-08-10 17:05:39 +0530

    Renamed Index.php to index.php physically so had to change link in logout.html

--- logout.php
+++ logout.php
@@ -5,7 +5,7 @@ session_start(); //Start the current session
 session_unset();
 session_destroy(); //Destroy it! So we are logged out now
 unset($_SESSION);
- header("location:Index.php?msg=Successfully Logged out");
+ header("location:index.php?msg=Successfully Logged out");
 //clearsessionscookies(); 
 break;
 exit();
